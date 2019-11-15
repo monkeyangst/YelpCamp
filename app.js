76f55e9c1a397aa7a99f4b8cab8fe5a8f5d2e5e8
Brad Hawkins <github@monkeylaw.org>
2019-11-15 12:56:45 -0600

    Changed port and IP to prepare for deployment

--- app.js
+++ app.js
@@ -60,6 +60,6 @@ app.use("/campgrounds/:id/comments", commentRoutes);
 app.use("/campgrounds", campgroundRoutes);
 
 // Server
-app.listen(3000, function() {
-    console.log("The YelpCamp server has started");
+app.listen(process.env.PORT, process.env.IP, function() {
+    console.log("The YelpCamp server has started on port " + this.address().port);
 });
